eccfe82e038ca3b25416102720b661541ea49dc7
slutske22 <seth.lutske@gmail.com>
2021-12-17 14:13:58 -0800

    Bringing code from node-algorithm into fuelmodels repo

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/*
+ * Firestarter.io
+ *
+ * Copyright (C) 2021 Blue Ohana, Inc.
+ * All rights reserved.
+ * The information in this software is subject to change without notice and
+ * should not be construed as a commitment by Blue Ohana, Inc.
+ *
+ */
+
+export * from './FuelModel';
+export * from './fuelmodel13';
+export * from './fuelmodel40';
